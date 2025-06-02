5777c9fac65373d24eaec85aa612f89496555d29
mikonno1 <kiet.vph@gmail.com>
2025-06-03 01:17:09 +0700

    Add Netlify configuration and initial API function stub

---
+++ backend/netlify/functions/api.js
@@ -0,0 +1,8 @@
+   // This file would need to be expanded into a proper serverless function
+   // Basic stub to show structure
+   exports.handler = async function(event, context) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: "API not yet implemented for serverless"})
+    };
+  }
